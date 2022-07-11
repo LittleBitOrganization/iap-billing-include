8f0852d0e22742abd3d30c3dc6e441ac904d1043
ariatophanes <ariatophanes@gmail.com>
2022-07-11 14:21:50 +0300

    Added adjust verification

--- Runtime/Services/PurchaseProcessors/CrossPlatformPurchaseHandler.cs
+++ Runtime/Services/PurchaseProcessors/CrossPlatformPurchaseHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using com.adjust.sdk.purchase;
 using LittleBit.Modules.IAppModule.Data;
 using UnityEngine;
 using UnityEngine.Purchasing;
@@ -15,28 +16,58 @@ namespace LittleBit.Modules.IAppModule.Services.PurchaseProcessors
         {
             _crossPlatformTangles = crossPlatformTangles;
         }
-        
+
         public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs args,
             Action<bool> callback)
         {
             try
             {
-                var validator =
-                    new CrossPlatformValidator(_crossPlatformTangles.GetGoogleData(), _crossPlatformTangles.GetAppleData(), Application.identifier);
+                var validator = new CrossPlatformValidator(_crossPlatformTangles.GetGoogleData(),
+                    _crossPlatformTangles.GetAppleData(), Application.identifier);
 
                 validator.Validate(args.purchasedProduct.receipt);
 
-                callback?.Invoke(true);
-                
+#if UNITY_IOS
+                AdjustPurchase.VerifyPurchaseiOS(args.purchasedProduct.receipt,
+                    args.purchasedProduct.transactionID,
+                    args.purchasedProduct.definition.storeSpecificId,
+                    delegate(ADJPVerificationInfo info) { VerificationInfoDelegate(info, callback); });
+
+#endif
+
+#if UNITY_ANDROID
+                AdjustPurchase.VerifyPurchaseAndroid(
+                    args.purchasedProduct.definition.id,
+                    args.purchasedProduct.transactionID,
+                    "hello, world",
+                    delegate(ADJPVerificationInfo info) { VerificationInfoDelegate(info, callback); });
+#endif
+
                 return PurchaseProcessingResult.Complete;
             }
             catch (IAPSecurityException ex)
             {
                 Debug.LogError("Invalid receipt: " + ex);
-                
+
                 callback?.Invoke(false);
 
                 return PurchaseProcessingResult.Complete;
+            }
+        }
+
+        private void VerificationInfoDelegate(ADJPVerificationInfo verificationInfo, Action<bool> purchaseCallback)
+        {
+            switch (verificationInfo.VerificationState)
+            {
+                case null:
+                    purchaseCallback?.Invoke(false);
+                    return;
+                case ADJPVerificationState.ADJPVerificationStatePassed:
+                    purchaseCallback?.Invoke(true);
+                    return;
+                default:
+                    purchaseCallback?.Invoke(false);
+                    break;
             }
         }
     }
