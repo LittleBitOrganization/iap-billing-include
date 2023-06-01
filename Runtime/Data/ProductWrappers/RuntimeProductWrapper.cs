2cffc273d76a62f072e8181482a490a63d74e648
plussamara <alligator85@list.ru>
2023-06-01 19:02:57 +0400

    add product params

--- Runtime/Data/ProductWrappers/RuntimeProductWrapper.cs
+++ Runtime/Data/ProductWrappers/RuntimeProductWrapper.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using UnityEngine;
 using UnityEngine.Purchasing;
 
 namespace LittleBit.Modules.IAppModule.Data.ProductWrappers
@@ -35,6 +37,74 @@ namespace LittleBit.Modules.IAppModule.Data.ProductWrappers
                 Id = product.definition.id,
                 Type = product.definition.type
             };
+        }
+        
+        public RuntimeProductWrapper(ProductParams product)
+        {
+            TransactionData = new()
+            {
+                HasReceiptGetter = () => product.HasReceipt,
+                ReceiptGetter = () => product.Receipt,
+                TransactionIdGetter = () => product.TransactionId
+            };
+
+            Metadata = new()
+            {
+                CurrencyCode = product.CurrencyCode,
+                CurrencySymbol = RegionInfo.CurrentRegion.CurrencySymbol,
+                LocalizedDescription = product.LocalizedDescription,
+                LocalizedPrice = product.LocalizedPrice,
+                LocalizedPriceString = product.LocalizedPriceString,
+                LocalizedTitle = product.LocalizedTitle,
+                CanPurchaseGetter = () =>
+                    Definition.Type.Equals(ProductType.Consumable) ||
+                    !Metadata.IsPurchasedGetter.Invoke(),
+                IsPurchasedGetter = () => PlayerPrefs.GetInt("MixPurchaseProduct"+Definition.Id,0)>0
+            };
+
+            Definition = new()
+            {
+                Id = product.Id,
+                Type = ConvertProductType(product.Type)
+            };
+        }
+        private ProductType ConvertProductType(int itemType)
+        {
+            switch (itemType)
+            {
+                case 0: { return ProductType.Consumable;    }
+                case 1: { return ProductType.NonConsumable; }
+                case 2: { return  ProductType.Subscription; }
+                default: {
+                    Debug.LogErrorFormat("Can not find the itemType:{0};", itemType);
+                    return ProductType.Consumable;
+                }
+            }
+        }
+        public void Purchase()
+        {
+            PlayerPrefs.SetInt("MixPurchaseProduct"+Definition.Id,1);
+        }
+    }
+    public class ProductParams
+    {
+        public string Id;
+        public bool HasReceipt = true;
+        public string Receipt = "Receipt";
+        public string TransactionId = "TransactionId";
+        public string CurrencyCode = "USD";
+        public string CurrencySymbol = "$";
+        public string LocalizedDescription = "LocalizedDescription";
+        public decimal LocalizedPrice = 10;
+        public string LocalizedPriceString = "LocalizedPriceString";
+        public string LocalizedTitle = "LocalizedTitle";
+        public int Type = 0;
+
+        public ProductParams(string id, int type, decimal price)
+        {
+            Id = id;
+            Type = type;
+            LocalizedPrice = price;
         }
     }
 }
